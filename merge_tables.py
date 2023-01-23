b5629b28c3e1eecc5e5519ea37fd988e4b98d632
ZeroRin <14952735+ZeroRin@users.noreply.github.com>
2023-01-23 14:04:42 +0800

    rename to index

--- merge_tables.py
+++ merge_tables.py
@@ -8,9 +8,11 @@ table_names = ["equip", "fairy", "gun_nm", "gun_sp"]
 for fname in table_names:
     tables = []
     for fp in log_dir.glob(f"*/{fname}.csv"):
+        print(f"reading {fp}")
         table_id = int(fp.parent.name[-1])
         df = pd.read_csv(fp)
         df["table"] = table_id
         tables.append(df.set_index(["table", "id"]))
+    print(f"merging {fname}")
     table = pd.concat(tables).sort_values("dev_time", kind="mergesort")
     table.to_csv(f"records/{fname}.csv")
