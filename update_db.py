60948a7ab092a248615c35034d67370f9b963945
ZeroRin <14952735+ZeroRin@users.noreply.github.com>
2023-07-28 13:45:58 +0800

    script for updating database

---
+++ update_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+import pandas as pd
+from gf_utils2.gamedata.gamedata import special_keys
+from gf_utils.stc_data import get_stc_data
+
+gamedata = get_stc_data(
+    ["../GF_Data_Tools/data/ch/stc", "../GF_Data_Tools/data/ch/catchdata"],
+    "../GF_Data_Tools/data/ch/asset/table",
+)
+with sqlite3.connect("../Elisa/logs/develop_log.db") as con:
+    for k in gamedata:
+        if not gamedata[k]:
+            continue
+        if isinstance(gamedata[k], list):
+            data = pd.DataFrame(gamedata[k], index=special_keys.get(k, "id"))
+        else:
+            data = pd.DataFrame(gamedata[k].values())
+        idx_key = special_keys.get(k, "id")
+        data.to_sql(k, con, if_exists="replace", index=False)
