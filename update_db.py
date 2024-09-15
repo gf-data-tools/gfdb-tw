ecab4ea0fb1ea83f52d13b5e74040a9eabe8a83a
ZeroRin <pptxp@qq.com>
2024-09-16 04:39:07 +0800

    ignore some errors

--- update_db.py
+++ update_db.py
@@ -9,13 +9,16 @@ gamedata = get_stc_data(
     "../GF_Data_Tools/data/ch/asset/table",
 )
 with sqlite3.connect("../Elisa/logs/develop_log.db") as con:
-    for k in gamedata:
-        print(k)
-        if not gamedata[k]:
-            continue
-        if isinstance(gamedata[k], list):
-            data = pd.DataFrame(gamedata[k], index=special_keys.get(k, "id"))
-        else:
-            data = pd.DataFrame(gamedata[k].values())
-        idx_key = special_keys.get(k, "id")
-        data.to_sql(k, con, if_exists="replace", index=False)
+    try:
+        for k in gamedata:
+            print(k)
+            if not gamedata[k]:
+                continue
+            if isinstance(gamedata[k], list):
+                data = pd.DataFrame(gamedata[k], index=special_keys.get(k, "id"))
+            else:
+                data = pd.DataFrame(gamedata[k].values())
+            idx_key = special_keys.get(k, "id")
+            data.to_sql(k, con, if_exists="replace", index=False)
+    except Exception as e:
+        print(e)
